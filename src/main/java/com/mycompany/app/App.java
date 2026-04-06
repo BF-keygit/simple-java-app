34e374663a5f532dcafb7df626b705c1f854efc0
Kelvin Gan <bo-fei.gan@keysight.com>
2026-04-06 15:51:35 +0800

    My third change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("2nd is for loser!");
+        System.out.println("3rd times the charm!");
     }
 
     public String getMessage() {
