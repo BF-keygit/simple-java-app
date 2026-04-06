903601f963911d32ba38f2d7caeae4421a6963ae
Kelvin Gan <bo-fei.gan@keysight.com>
2026-04-06 16:09:40 +0800

    Make a change to demonstrate Poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("3rd times the charm!");
+        System.out.println("Well now I have to change to test Poll SCM");
     }
 
     public String getMessage() {
