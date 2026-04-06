6347eb13cc2dbe2b5b976d554acfd897df51e5b3
Kelvin Gan <bo-fei.gan@keysight.com>
2026-04-06 15:43:31 +0800

    My first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("This training is boring. Give me more challenge :)");
+        System.out.println("Modified from local project!");
     }
 
     public String getMessage() {
