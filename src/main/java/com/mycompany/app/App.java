a41eb6b1f9fb17e5142e1e0523228df0d88031d4
Kelvin Gan <bo-fei.gan@keysight.com>
2026-04-06 16:38:17 +0800

    Some changes to test webhooks

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Playing with Chain!");
+        System.out.println("Webhooks worked perfectly");
     }
 
     public String getMessage() {
